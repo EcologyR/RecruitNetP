ac3ae9dc3946e1e56a803177b6d45d61c0db21ae
Alicia Montesinos <ali.montesinos@gmail.com>
2023-03-23 10:32:08 +0100

    add multiplication function

---
+++ R/multiplicacion.R
@@ -0,0 +1,16 @@
+#' Multiplication
+#'
+#' @param x First Number
+#' @param y Second Number
+#'
+#' @return Integer
+#' @export
+#'
+#' @examples
+
+#'multiply(2,3)
+
+
+multiply <- function(x = NULL, y = NULL) {
+  x*y
+}
