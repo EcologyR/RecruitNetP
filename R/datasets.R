aab99a29a565f287d7c3391893536cf6f5d754e4
jlgarridosan <jlgarridosan@gmail.com>
2023-03-23 13:42:25 +0100

    Anadida base de datos como binario

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+#' RecruitNet
+#'
+#' Plant-plant interactions global database
+#'
+#' @source https://doi.org/10.1002/ecy.3923
+"RN"
+
+#' RecruitNet-Cover
+#'
+#' Canopy cover of plant-plant interactions global database
+#'
+#' @source https://doi.org/10.1002/ecy.3923
+"RNCover"
