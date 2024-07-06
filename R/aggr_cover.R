c3770b93bb40d76d626d37d8bde3487b9d82bbb7
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-06 20:09:55 +0200

    add aggr_cover

---
+++ R/aggr_cover.R
@@ -0,0 +1,24 @@
+#' Calculate aggregated species cover
+#'
+#' Calculate the aggregated (i.e. summed) cover of each species across
+#' all the plots from a local community.
+#'
+#' @param dataset character. Name of the data set where cover data are stored.
+#' Usually, it will be "CanopyCover" (or the name assigned to the
+#' dataset "CanopyCover.csv" when it was imported).
+#' @param site character. Name of a study site.
+#'
+#' @return A data frame containing the total cover of each species in a local community,
+#' aggregated across plots. It has two columns: "Canopy", with the name of each species (irrespective
+#' of being acting as canopies or recruits); and "abundance".
+#'
+#' @examples
+#' Ventisquero_cover <- aggr_cover(CanopyCover, site = "Ventisquero")
+
+aggr_cover <- function(dataset, site) {
+  cover <- dataset[which(dataset$Study_site == site), ]
+  cover$abundance <- (cover$Cover / 100) * cover$Sampled_distance_or_area
+  cover <- aggregate(abundance ~ Canopy, data = cover, sum)
+  return(cover)
+
+}
