1b8f9e5ede4459e73c1b26d6313f3612bdc37e04
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-07 18:20:22 +0200

    delete old tests

--- tests/testthat/test-intertest.R
+++
@@ -1,6 +0,0 @@
-test_that("the input has the right format", {
-  popo<-data.frame(A=4, Canopy_Freq=3)
-  expect_error (intertest(db_inter=popo, iteration=100, threshold=5)) #esto debe de dar error porque no tiene el nombre de a svariables que yo quiero
-
-
-})
