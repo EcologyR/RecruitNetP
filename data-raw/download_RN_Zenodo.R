4e4917a7d5880d72ca2057a37eb064d81cf777ba
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-11 16:34:36 +0200

    clean up

--- data-raw/download_RN_Zenodo.R
+++ data-raw/download_RN_Zenodo.R
@@ -1,6 +1,8 @@
-download.file("https://zenodo.org/record/6567608/files/Data_S1.zip?download=1", "data-raw/RNDB.v4.zip", mode = "wb")
+download.file("https://zenodo.org/record/6567608/files/Data_S1.zip?download=1",
+              "data-raw/RNDB.v4.zip", mode = "wb")
 
 unzip("data-raw/RNDB.v4.zip", exdir = "data-raw")
+file.remove("data-raw/RNDB.v4.zip")
 
 RN <- as.data.frame(readr::read_csv("data-raw/RecruitNet.csv"))
 RNCover <- as.data.frame(readr::read_csv("data-raw/CanopyCover.csv"))
