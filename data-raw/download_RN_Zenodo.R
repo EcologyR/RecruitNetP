5a5945f5125e29b84e92cef135caf5843e32d003
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-11 17:41:09 +0200

    fix encoding problems in RN database

--- data-raw/download_RN_Zenodo.R
+++ data-raw/download_RN_Zenodo.R
@@ -4,10 +4,18 @@ download.file("https://zenodo.org/record/6567608/files/Data_S1.zip?download=1",
 unzip("data-raw/RNDB.v4.zip", exdir = "data-raw")
 file.remove("data-raw/RNDB.v4.zip")
 
-RecruitNet <- as.data.frame(readr::read_csv("data-raw/RecruitNet.csv"))
-CanopyCover <- as.data.frame(readr::read_csv("data-raw/CanopyCover.csv"))
 
+RecruitNet <- readr::read_csv("data-raw/RecruitNet.csv",
+                              locale = locale(encoding = "ISO-8859-1"))
+readr::write_csv(RecruitNet, "data-raw/RecruitNet.csv")
+RecruitNet <- as.data.frame(readr::read_csv("data-raw/RecruitNet.csv"))
 usethis::use_data(RecruitNet, overwrite = TRUE)
+
+
+CanopyCover <- as.data.frame(readr::read_csv("data-raw/CanopyCover.csv"),
+                             locale = locale(encoding = "ISO-8859-1"))
+readr::write_csv(CanopyCover, "data-raw/CanopyCover.csv")
+CanopyCover <- as.data.frame(readr::read_csv("data-raw/CanopyCover.csv"))
 usethis::use_data(CanopyCover, overwrite = TRUE)
 
 # data("RecruitNet")
