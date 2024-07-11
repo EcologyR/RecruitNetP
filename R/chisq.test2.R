cf7d0d94a3859e51e6ba4df2b13a028ee480d8e7
aliciamontesinos <40610362+aliciamontesinos@users.noreply.github.com>
2024-07-11 12:39:47 +0200

    Add files via upload

---
+++ R/chisq.test2.R
@@ -0,0 +1,154 @@
+#' @param x a numeric vector or matrix. `x` and `y` can also both be factors.
+#' @param y a numeric vector; ignored if `x` is a matrix. `x` and `y` can also both be factors.
+#' @param correct a logical indicating whether to apply continuity correction in the case of a 2x2 table. Default is TRUE.
+#' @param p a vector of probabilities of the same length as `x`, used for goodness-of-fit tests. Default is rep(1/length(x), length(x)).
+#' @param rescale.p a logical indicating whether to rescale `p` to sum to 1. Default is FALSE.
+#' @param simulate.p.value a logical indicating whether to compute p-values by Monte Carlo simulation. Default is FALSE.
+#' @param B an integer specifying the number of replicates used in the Monte Carlo test. Default is 2000.
+#'
+#' @return A list with class "htest".
+#' @details
+#' The original function chisq.test performs a chi-squared test of the null hypothesis that the
+#' proportions in a one-way table (goodness-of-fit) or the counts in a two-way
+#' table (contingency) follow a specified distribution.
+#' chisq.test2 is a modified version of chisq.test function that optimices the use of its argument simulated.p.value =T
+#' This version uses tabulate instead of table to create frequency counts
+#'
+#' @examples
+#' ## Goodness-of-fit test
+#' chisq.test(c(4, 6, 17, 16, 8, 9), p = c(1, 1, 1, 1, 1, 1)/6)
+
+
+chisq.test2 <-function (x, y = NULL, correct = TRUE, p = rep(1/length(x), length(x)), 
+                        rescale.p = FALSE, simulate.p.value = FALSE, B = 2000) 
+{
+  DNAME <- deparse(substitute(x))
+  if (is.data.frame(x)) 
+    x <- as.matrix(x)
+  if (is.matrix(x)) {
+    if (min(dim(x)) == 1L) 
+      x <- as.vector(x)
+  }
+  if (!is.matrix(x) && !is.null(y)) {
+    if (length(x) != length(y)) 
+      stop("'x' and 'y' must have the same length")
+    DNAME2 <- deparse(substitute(y))
+    xname <- if (length(DNAME) > 1L || nchar(DNAME, "w") > 
+                 30) 
+      ""
+    else DNAME
+    yname <- if (length(DNAME2) > 1L || nchar(DNAME2, "w") > 
+                 30) 
+      ""
+    else DNAME2
+    OK <- complete.cases(x, y)
+    x <- factor(x[OK])
+    y <- factor(y[OK])
+    if ((nlevels(x) < 2L) || (nlevels(y) < 2L)) 
+      stop("'x' and 'y' must have at least 2 levels")
+    x <- table(x, y)
+    names(dimnames(x)) <- c(xname, yname)
+    DNAME <- paste(paste(DNAME, collapse = "\n"), "and", 
+                   paste(DNAME2, collapse = "\n"))
+  }
+  if (any(x < 0) || anyNA(x)) 
+    stop("all entries of 'x' must be nonnegative and finite")
+  if ((n <- sum(x)) == 0) 
+    stop("at least one entry of 'x' must be positive")
+  if (simulate.p.value) {
+    setMETH <- function() METHOD <<- paste(METHOD, "with simulated p-value\n\t (based on", 
+                                           B, "replicates)")
+    almost.1 <- 1 - 64 * .Machine$double.eps
+  }
+  if (is.matrix(x)) {
+    METHOD <- "Pearson's Chi-squared test"
+    nr <- as.integer(nrow(x))
+    nc <- as.integer(ncol(x))
+    if (is.na(nr) || is.na(nc) || is.na(nr * nc)) 
+      stop("invalid nrow(x) or ncol(x)", domain = NA)
+    sr <- rowSums(x)
+    sc <- colSums(x)
+    E <- outer(sr, sc)/n
+    v <- function(r, c, n) c * r * (n - r) * (n - c)/n^3
+    V <- outer(sr, sc, v, n)
+    dimnames(E) <- dimnames(x)
+    if (simulate.p.value && all(sr > 0) && all(sc > 0)) {
+      setMETH()
+      tmp <- .Call(C_chisq_sim, sr, sc, B, E)
+      STATISTIC <- sum(sort((x - E)^2/E, decreasing = TRUE))
+      PARAMETER <- NA
+      PVAL <- (1 + sum(tmp >= almost.1 * STATISTIC))/(B + 
+                                                        1)
+    }
+    else {
+      if (simulate.p.value) 
+        warning("cannot compute simulated p-value with zero marginals")
+      if (correct && nrow(x) == 2L && ncol(x) == 2L) {
+        YATES <- min(0.5, abs(x - E))
+        if (YATES > 0) 
+          METHOD <- paste(METHOD, "with Yates' continuity correction")
+      }
+      else YATES <- 0
+      STATISTIC <- sum((abs(x - E) - YATES)^2/E)
+      PARAMETER <- (nr - 1L) * (nc - 1L)
+      PVAL <- pchisq(STATISTIC, PARAMETER, lower.tail = FALSE)
+    }
+  }
+  else {
+    if (length(dim(x)) > 2L) 
+      stop("invalid 'x'")
+    if (length(x) == 1L) 
+      stop("'x' must at least have 2 elements")
+    if (length(x) != length(p)) 
+      stop("'x' and 'p' must have the same number of elements")
+    if (any(p < 0)) 
+      stop("probabilities must be non-negative.")
+    if (abs(sum(p) - 1) > sqrt(.Machine$double.eps)) {
+      if (rescale.p) 
+        p <- p/sum(p)
+      else stop("probabilities must sum to 1.")
+    }
+    METHOD <- "Chi-squared test for given probabilities"
+    E <- n * p
+    V <- n * p * (1 - p)
+    STATISTIC <- sum((x - E)^2/E)
+    names(E) <- names(x)
+    #     if (simulate.p.value) {
+    #         setMETH()
+    #        nx <- length(x)
+    #         sm <- matrix(sample.int(nx, B * n, TRUE, prob = p), 
+    #             nrow = n)
+    #         ss <- apply(sm, 2L, function(x, E, k) {
+    #             sum((table(factor(x, levels = 1L:k)) - E)^2/E)
+    #         }, E = E, k = nx)
+    #         PARAMETER <- NA
+    #         PVAL <- (1 + sum(ss >= almost.1 * STATISTIC))/(B + 
+    #             1)
+    #      }
+    
+    if (simulate.p.value) {
+      setMETH()
+      nx <- length(x)
+      sm <- matrix(sample.int(nx, B * n, TRUE, prob = p), nrow = n)
+      ss <- apply(sm, 2L, function(col) {
+        freq_table <- tabulate(col, nbins = nx)
+        sum((freq_table - E)^2 / E)
+      })
+      PARAMETER <- NA
+      PVAL <- (1 + sum(ss >= almost.1 * STATISTIC)) / (B + 1)
+    }
+    
+    else {
+      PARAMETER <- length(x) - 1
+      PVAL <- pchisq(STATISTIC, PARAMETER, lower.tail = FALSE)
+    }
+  }
+  names(STATISTIC) <- "X-squared"
+  names(PARAMETER) <- "df"
+  if (any(E < 5) && is.finite(PARAMETER)) 
+    warning("Chi-squared approximation may be incorrect")
+  structure(list(statistic = STATISTIC, parameter = PARAMETER, 
+                 p.value = PVAL, method = METHOD, data.name = DNAME, observed = x, 
+                 expected = E, residuals = (x - E)/sqrt(E), stdres = (x - 
+                                                                        E)/sqrt(V)), class = "htest")
+}
---
+++ R/sigtestMV.R
@@ -0,0 +1,116 @@
+#' Calculate a significance tests of association for each row. Rows can be a pair-wise interaction (Recruit species-Canopy species interaction), the number of recruits observed under any canopy species (Recruit species) or the number of Recruits of any given speies under a given canopy species (Canopy species)
+#'
+#' @param db_inter data frame obtained as the output of the functions pre_asocindex, recruit_level,canopy_level, or com_level
+#' @param iteration number of iterations
+#' @param threshold minimun number of recruits to perform the binomial test
+#'
+#' @return data frame with the same structure as the input with three additional columns:int_p (p-value of the binomial test of association),int_sign (the sign of the association, being Posive (or Negative) if the association is stronger (or weaker) than expected by the percentage cover of Canopy and Open, and Neutral if there is not enougth power to conduct the test, stdres (standarized resdual quantifying the difference between the observed and expected values), and testability (indicating whether the sample size allow to conduct or not the test being Non-testable those rows in which int_sign = Neutral)  
+#' @export
+#'
+#' @examples sigtest(com_level (RecruitNet, CanopyCover)) o sigtest(pre_asocindex(RecruitNet, CanopyCover)) "very slow"
+
+sigtestMV <- function(data_freq=db_inter,
+                      iteration = 100,
+                      threshold = 5) {
+  
+  db_inter<-data_freq
+  
+  
+  db_inter$int_p <- rep(NA, dim(db_inter)[1])
+  db_inter$int_sign <- rep(NA, dim(db_inter)[1])
+  
+  for (i in 1:dim(db_inter)[1]) {
+    if (sum(db_inter$Canopy_Freq[i] + db_inter$Open_Freq[i]) < 100000) {
+      test <-
+        chisq.test2(
+          c(db_inter$Canopy_Freq[i], db_inter$Open_Freq[i]),
+          p = c(db_inter$Canopy_cover[i], db_inter$Open_cover[i]),
+          rescale.p = TRUE,
+          simulate.p.value = TRUE
+        )
+      db_inter$int_p[i] <- test$p.value
+      db_inter$stdres[i] <- test$stdres[1]
+      db_inter$int_sign[i] <-
+        ifelse(
+          test$p.value <= 0.05 &
+            test$stdres[1] > 0,
+          "Positive",
+          ifelse(
+            test$p.value <= 0.05 & test$stdres[1] < 0,
+            "Negative",
+            "Neutral"
+          )
+        )
+    }
+    
+    if (sum(db_inter$Canopy_Freq[i] + db_inter$Open_Freq[i]) >= 100000) {
+      test <-
+        chisq.test2(
+          c(db_inter$Canopy_Freq[i], db_inter$Open_Freq[i]),
+          p = c(db_inter$Canopy_cover[i], db_inter$Open_cover[i]),
+          rescale.p = TRUE,
+          simulate.p.value = FALSE
+        )
+      db_inter$int_p[i] <- test$p.value
+      db_inter$stdres[i] <- test$stdres[1]
+      db_inter$int_sign[i] <-
+        ifelse(
+          test$p.value <= 0.05 &
+            test$stdres[1] > 0,
+          "Positive",
+          ifelse(
+            test$p.value <= 0.05 & test$stdres[1] < 0,
+            "Negative",
+            "Neutral"
+          )
+        )
+    }
+    
+  }
+  
+  
+  db_inter$testability <- rep("NA", dim(db_inter)[1])
+  
+  for (i in 1:dim(db_inter)[1])
+  {
+    if (db_inter[i, ]$int_sign != "Neutral") {
+      db_inter[i, ]$testability <- db_inter[i, ]$int_sign
+    }
+    
+    if (db_inter[i, ]$int_sign == "Neutral" &
+        sum(db_inter[i, ]$Canopy_Freq + db_inter[i, ]$Open_Freq) > 5) {
+      db_inter[i, ]$testability <- db_inter[i, ]$int_sign
+    }
+    
+    if (db_inter[i, ]$int_sign == "Neutral" &
+        sum(db_inter[i, ]$Canopy_Freq + db_inter[i, ]$Open_Freq) < 6) {
+      n <- sum(db_inter[i, ]$Canopy_Freq + db_inter[i, ]$Open_Freq)
+      fcan <-
+        ifelse(db_inter[i, ]$Canopy_cover <= db_inter[i, ]$Open_cover, n, 0)
+      fopen <-
+        ifelse(db_inter[i, ]$Open_cover < db_inter[i, ]$Canopy_cover, n, 0)
+      
+      db_inter[i, ]$testability <-
+        ifelse(length(which(
+          replicate(
+            iteration,
+            chisq.test2(
+              c(fcan, fopen),
+              p = c(db_inter$Canopy_cover[i], db_inter$Open_cover[i]),
+              rescale.p = T,
+              simulate.p = T
+            )$p.value
+          ) < 0.05
+        )) > threshold, "testable", "non_testable")
+    }
+    
+    db_inter[db_inter$testability == "testable", "testability"] <-
+      "Neutral"
+  }
+  
+  
+  
+  return(db_inter)
+  
+  
+}
