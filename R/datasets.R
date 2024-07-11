f5af616a6477b8a2830dac5c05c5c5a8164e7305
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-11 18:20:52 +0200

    change dataset names

--- R/datasets.R
+++ R/datasets.R
@@ -3,11 +3,11 @@
 #' Plant-plant interactions global database
 #'
 #' @source https://doi.org/10.1002/ecy.3923
-"RN"
+"RecruitNet"
 
 #' RecruitNet-Cover
 #'
 #' Canopy cover of plant-plant interactions global database
 #'
 #' @source https://doi.org/10.1002/ecy.3923
-"RNCover"
+"CanopyCover"
