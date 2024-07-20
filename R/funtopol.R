a11061ea5ad254d2dedc979170b9a2a0c5e3e450
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-20 13:05:42 +0200

    revise funtopol

--- R/funtopol.R
+++ R/funtopol.R
@@ -6,10 +6,11 @@
 #' Alcantara & Rey (2012) derived a qualitative way to infer potential species
 #' persistence from their position in the RNs based on a combination of non-negative
 #' matrix theory and graph theory. Basically, directed unipartite graphs can be
-#' unambiguously dissected into SCCs. SCCs are the largest possible subgroups of
+#' unambiguously dissected into 'SCCs'. SCCs are the largest possible subgroups of
 #' nodes connected so that all the nodes in a subgroup can be reached from all others
 #' following the directions of the links. In the case of RNs, we can define five
 #' types of SCCs which play different functional roles:
+#'
 #' - The "core" of the network is the SCC formed by the largest number of species.
 #'   All species in the core must recruit, at least, in the vicinity of another core
 #'   species, and allow the recruitment of at least another core species.
@@ -22,6 +23,7 @@
 #'   recruit away from established plants).
 #' - "Strict transients" are species that cannot be reached from any other node (i.e.
 #'   those that do not recruit in the studied local assemblage).
+#'
 #' Assuming that the dynamics of the system is linear and time-invariant (LTI dynamics),
 #' like in Markov models (Horn, 1975, Siles et al., 2008), only core and satellite
 #' species will persist in equilibrium in the absence of disturbance. When the dynamics
@@ -41,55 +43,57 @@
 #' @export
 #'
 #' @examples
-#' int_data <- read.csv("RecruitNet.csv")
+#' data(RecruitNet)
 #' int_data <- comm_subset(RecruitNet, site = "Ventisquero")
 #' Ventisquero_funtopol <- funtopol(int_data)
 #' Ventisquero_funtopol$Descriptors
 #' Ventisquero_funtopol$Functional_classification
 
 
-funtopol<-function(int_data){
+funtopol <- function(int_data){
 
-  if ("Canopy" %in% names(int_data) == FALSE) stop("ERROR: your interactions data lacks a column named: Canopy")
-  if ("Recruit" %in% names(int_data) == FALSE) stop("ERROR: your interactions data lacks a column named: Recruit")
-  if ("Open" %in% int_data$Canopy == FALSE) stop("ERROR: your data does not contain a node named Open or it is spelled differently.")
+  if (!"Canopy" %in% names(int_data)) stop("ERROR: your interactions data lacks a column named: Canopy")
+  if (!"Recruit" %in% names(int_data)) stop("ERROR: your interactions data lacks a column named: Recruit")
+  if (!"Open" %in% int_data$Canopy) stop("ERROR: your data does not contain a node named Open or it is spelled differently.")
 
   int_data <- int_data[c("Canopy", "Recruit")]
   g <- igraph::graph_from_data_frame(int_data, directed = TRUE)
   g <- igraph::simplify(g, remove.multiple = TRUE, remove.loops = FALSE)
   NEdges <- igraph::gsize(g)
   NNodes <- igraph::gorder(g)
-  CDirected <- NEdges/(NNodes*(NNodes-1))
-  SCCs <- igraph::components(g, mode="strong")
+  CDirected <- NEdges/(NNodes*(NNodes - 1))
+  SCCs <- igraph::components(g, mode = "strong")
   numSCCs <- SCCs$no
   numNTSCCs <- sum(SCCs$csize > 1)
   coreSize <- max(SCCs$csize)
   SCC_memb <- SCCs$membership
   SCC_memb <- as.data.frame(SCC_memb)
-  SCC_subgraphs <- igraph::decompose(g, mode="strong") # Makes a subgraph of each SCC
+  SCC_subgraphs <- igraph::decompose(g, mode = "strong") # Makes a subgraph of each SCC
   IDcore <- match(coreSize, SCCs$csize) # locates the position of the core in the list of SCCs
   MembersCore <- igraph::V(SCC_subgraphs[[IDcore]])$name # List of the species in the core
   IDOpen <- SCC_memb$SCC_memb[match("Open", row.names(SCC_memb))] # Locate the position of the "open" node in the list of SCCs
   outReachFromOpen <- names(igraph::subcomponent(g, "Open", "out")[-1]) # List of nodes reachable from the "open"
-  outReachFromCore <- vector("list",coreSize) # List of nodes reachable from core nodes
-  for(i in 1:coreSize) {outReachFromCore[[i]] <- igraph::subcomponent(g, MembersCore[i], mode="out")}
+  outReachFromCore <- vector("list", coreSize) # List of nodes reachable from core nodes
+  for (i in 1:coreSize) {
+    outReachFromCore[[i]] <- igraph::subcomponent(g, MembersCore[i], mode = "out")
+  }
   a <- unlist(outReachFromCore)
   a <- unique(names(a))
   MembersSatellites <- setdiff(a, MembersCore)
   MembersTransients <- setdiff(igraph::V(g)$name,c(MembersCore,MembersSatellites))
-  MembersTransients <- MembersTransients[!MembersTransients=="Open"]
+  MembersTransients <- MembersTransients[!MembersTransients == "Open"]
   MembersStrictTransients <- setdiff(MembersTransients, outReachFromOpen)
   MembersDdTransients <- setdiff(MembersTransients, MembersStrictTransients)
   numSat <- length(MembersSatellites)
   numTransAll <- length(MembersTransients)
   numDdTrans <- length(MembersDdTransients)
   numStrictTrans <- length(MembersStrictTransients)
-  propCore <- coreSize/(NNodes-1)
-  propSat <- numSat/(NNodes-1)
-  propTrans <- numTransAll/(NNodes-1)
-  propStrTrans <- numStrictTrans/(NNodes-1)
-  propDdTrans <- numDdTrans/(NNodes-1)
-  persistence <- propCore+propSat
+  propCore <- coreSize/(NNodes - 1)
+  propSat <- numSat/(NNodes - 1)
+  propTrans <- numTransAll/(NNodes - 1)
+  propStrTrans <- numStrictTrans/(NNodes - 1)
+  propDdTrans <- numDdTrans/(NNodes - 1)
+  persistence <- propCore + propSat
 
   # Function output
 
@@ -129,13 +133,15 @@ funtopol<-function(int_data){
     MembersCore,
     MembersStrictTransients,
     MembersDdTransients)
-  classif <- setNames(classif, c("Satellites",
+  classif <- stats::setNames(classif,
+                             c("Satellites",
                                  "Core",
                                  "Strict_transients",
                                  "Disturbance_dependent_transients")
   )
 
-  outputs<-list("Descriptors" = df, "Functional_classification" = classif)
+  outputs <- list("Descriptors" = df, "Functional_classification" = classif)
 
   return(outputs)
+
 }
