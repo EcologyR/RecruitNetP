d87b685c78e8ca6b9950134d40cee88144401e19
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2026-02-18 17:04:45 +0100

    delete old files

--- R/globals.R
+++
@@ -1,4 +0,0 @@
-utils::globalVariables(c("C_chisq_sim", "Canopy", "CanopyCover", "Canopy_Freq",
-                         "Canopy_cover", "Canopy_cover_abs", "Frequency",
-                         "Open_Freq", "Open_cover", "Recruit", "RecruitNet",
-                         "Sampled_distance_or_area", "Study_site", "inter_ID"))
