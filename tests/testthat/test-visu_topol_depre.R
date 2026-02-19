f6c5fa102b8e8e0f47d5bb7e31c7f11807d0a841
aliciamontesinos <40610362+aliciamontesinos@users.noreply.github.com>
2026-02-19 23:02:56 +0100

    Update test-visu_topol_depre.R

--- tests/testthat/test-visu_topol_depre.R
+++ tests/testthat/test-visu_topol_depre.R
@@ -1,29 +1,13 @@
-#################################################
-#Tests para visu_topol_depre
-#################################################
 
-#library(testthat)
-
-#load data
-mypath<-getwd()
-download_RN() # Run only the first time you use the package.
-setwd(mypath)
-RecruitNet <-read.csv("RecruitNet.csv")
-CanopyCover <-read.csv("CanopyCover.csv")
-
-mysite_com <- comm_subset_UNI(RecruitNet, "Amoladeras")
-mysite_cov <- comm_subset_UNI(CanopyCover, "Amoladeras")
-
-#------------------------------------
 
 test_that("visu_topol_depre returns graph and SCC list", {
-  
+
   expect_silent({
     res <- visu_topol_depre(test_data$com,
                             test_data$cov,
                             direction = "out")
   })
-  
+
   expect_type(res, "list")
   expect_named(res, c("graph", "scc"))
   expect_s3_class(res$graph, "igraph")
@@ -32,29 +16,29 @@ test_that("visu_topol_depre returns graph and SCC list", {
 #------------------------------------
 
 test_that("Graph contains correct number of nodes", {
-  
+
   res <- suppressWarnings(
     visu_topol_depre(test_data$com,
                      test_data$cov,
                      direction = "out")
   )
-  
+
   expected_species <- unique(c(test_data$com$Canopy,
                                test_data$com$Recruit))
-  
+
   expect_equal(vcount(res$graph), length(expected_species))
 })
 
 #------------------------------------
 
 test_that("SCC detection works when loops exist", {
-  
+
   res <- suppressWarnings(
     visu_topol_depre(test_data$com,
                      test_data$cov,
                      direction = "out")
   )
-  
+
   # There should be at least one SCC with >1 node
   expect_true(length(res$scc) > 0)
   expect_true(all(sapply(res$scc, length) > 1))
@@ -63,13 +47,13 @@ test_that("SCC detection works when loops exist", {
 #------------------------------------
 
 test_that("Works with real dataset even if no loops exist", {
-  
+
   expect_silent({
     res <- visu_topol_depre(mysite_com,
                             mysite_cov,
                             direction = "out")
   })
-  
+
   expect_type(res$scc, "list")
 })
 
@@ -78,7 +62,7 @@ test_that("Works with real dataset even if no loops exist", {
 
 
 test_that("Custom layout function does not break plotting", {
-  
+
   expect_silent(
     visu_topol_depre(test_data$com,
                      test_data$cov,
@@ -91,7 +75,7 @@ test_that("Custom layout function does not break plotting", {
 #------------------------------------
 
 test_that("Custom vertex and edge parameters work", {
-  
+
   expect_silent(
     visu_topol_depre(test_data$com,
                      test_data$cov,
